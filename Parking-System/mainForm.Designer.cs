58acd7638e15b64d94b0422f7065fbce74da9bea
Lưu Văn Cụi <luuvancui10a1516@gmail.com>
2020-04-23 09:44:58 +0700

    Add mainForm

---
+++ Parking-System/mainForm.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Parking_System
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.xeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addVehicleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteVehicleByIDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageVerhicleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.staToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vehicleListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.workListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageWorkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.thợToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageAllWorkerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chuyênMônToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hợpĐồngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageContractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contractDetailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 19F);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.xeToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.thợToolStripMenuItem,
+            this.chuyênMônToolStripMenuItem,
+            this.hợpĐồngToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(858, 59);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // xeToolStripMenuItem
+            // 
+            this.xeToolStripMenuItem.BackColor = System.Drawing.Color.SpringGreen;
+            this.xeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addVehicleToolStripMenuItem,
+            this.deleteVehicleByIDToolStripMenuItem,
+            this.manageVerhicleToolStripMenuItem,
+            this.staToolStripMenuItem,
+            this.vehicleListToolStripMenuItem,
+            this.printToolStripMenuItem});
+            this.xeToolStripMenuItem.Name = "xeToolStripMenuItem";
+            this.xeToolStripMenuItem.Size = new System.Drawing.Size(158, 55);
+            this.xeToolStripMenuItem.Text = "Vehicle";
+            // 
+            // addVehicleToolStripMenuItem
+            // 
+            this.addVehicleToolStripMenuItem.Name = "addVehicleToolStripMenuItem";
+            this.addVehicleToolStripMenuItem.Size = new System.Drawing.Size(470, 60);
+            this.addVehicleToolStripMenuItem.Text = "Add Vehicle";
+            this.addVehicleToolStripMenuItem.Click += new System.EventHandler(this.addVehicleToolStripMenuItem_Click);
+            // 
+            // deleteVehicleByIDToolStripMenuItem
+            // 
+            this.deleteVehicleByIDToolStripMenuItem.Name = "deleteVehicleByIDToolStripMenuItem";
+            this.deleteVehicleByIDToolStripMenuItem.Size = new System.Drawing.Size(470, 60);
+            this.deleteVehicleByIDToolStripMenuItem.Text = "Delete Vehicle By ID";
+            this.deleteVehicleByIDToolStripMenuItem.Click += new System.EventHandler(this.deleteVehicleByIDToolStripMenuItem_Click);
+            // 
+            // manageVerhicleToolStripMenuItem
+            // 
+            this.manageVerhicleToolStripMenuItem.Name = "manageVerhicleToolStripMenuItem";
+            this.manageVerhicleToolStripMenuItem.Size = new System.Drawing.Size(470, 60);
+            this.manageVerhicleToolStripMenuItem.Text = "Manage Verhicle";
+            this.manageVerhicleToolStripMenuItem.Click += new System.EventHandler(this.manageVerhicleToolStripMenuItem_Click);
+            // 
+            // staToolStripMenuItem
+            // 
+            this.staToolStripMenuItem.Name = "staToolStripMenuItem";
+            this.staToolStripMenuItem.Size = new System.Drawing.Size(470, 60);
+            this.staToolStripMenuItem.Text = "Statistic";
+            this.staToolStripMenuItem.Click += new System.EventHandler(this.staToolStripMenuItem_Click);
+            // 
+            // vehicleListToolStripMenuItem
+            // 
+            this.vehicleListToolStripMenuItem.Name = "vehicleListToolStripMenuItem";
+            this.vehicleListToolStripMenuItem.Size = new System.Drawing.Size(470, 60);
+            this.vehicleListToolStripMenuItem.Text = "Vehicle List";
+            this.vehicleListToolStripMenuItem.Click += new System.EventHandler(this.vehicleListToolStripMenuItem_Click);
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(470, 60);
+            this.printToolStripMenuItem.Text = "Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.BackColor = System.Drawing.Color.Brown;
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.workListToolStripMenuItem,
+            this.manageWorkToolStripMenuItem,
+            this.printToolStripMenuItem1});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(127, 55);
+            this.toolStripMenuItem2.Text = "Work";
+            // 
+            // workListToolStripMenuItem
+            // 
+            this.workListToolStripMenuItem.Name = "workListToolStripMenuItem";
+            this.workListToolStripMenuItem.Size = new System.Drawing.Size(369, 60);
+            this.workListToolStripMenuItem.Text = "Work List";
+            // 
+            // manageWorkToolStripMenuItem
+            // 
+            this.manageWorkToolStripMenuItem.Name = "manageWorkToolStripMenuItem";
+            this.manageWorkToolStripMenuItem.Size = new System.Drawing.Size(369, 60);
+            this.manageWorkToolStripMenuItem.Text = "Manage Work";
+            // 
+            // printToolStripMenuItem1
+            // 
+            this.printToolStripMenuItem1.Name = "printToolStripMenuItem1";
+            this.printToolStripMenuItem1.Size = new System.Drawing.Size(369, 60);
+            this.printToolStripMenuItem1.Text = "Print";
+            // 
+            // thợToolStripMenuItem
+            // 
+            this.thợToolStripMenuItem.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.thợToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageAllWorkerToolStripMenuItem});
+            this.thợToolStripMenuItem.Name = "thợToolStripMenuItem";
+            this.thợToolStripMenuItem.Size = new System.Drawing.Size(160, 55);
+            this.thợToolStripMenuItem.Text = "Worker";
+            // 
+            // manageAllWorkerToolStripMenuItem
+            // 
+            this.manageAllWorkerToolStripMenuItem.Name = "manageAllWorkerToolStripMenuItem";
+            this.manageAllWorkerToolStripMenuItem.Size = new System.Drawing.Size(455, 60);
+            this.manageAllWorkerToolStripMenuItem.Text = "Manage All Worker";
+            // 
+            // chuyênMônToolStripMenuItem
+            // 
+            this.chuyênMônToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.chuyênMônToolStripMenuItem.Name = "chuyênMônToolStripMenuItem";
+            this.chuyênMônToolStripMenuItem.Size = new System.Drawing.Size(201, 55);
+            this.chuyênMônToolStripMenuItem.Text = "Specialize";
+            // 
+            // hợpĐồngToolStripMenuItem
+            // 
+            this.hợpĐồngToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.hợpĐồngToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageContractToolStripMenuItem,
+            this.printToolStripMenuItem2,
+            this.contractDetailToolStripMenuItem});
+            this.hợpĐồngToolStripMenuItem.Name = "hợpĐồngToolStripMenuItem";
+            this.hợpĐồngToolStripMenuItem.Size = new System.Drawing.Size(182, 55);
+            this.hợpĐồngToolStripMenuItem.Text = "Contract";
+            // 
+            // manageContractToolStripMenuItem
+            // 
+            this.manageContractToolStripMenuItem.Name = "manageContractToolStripMenuItem";
+            this.manageContractToolStripMenuItem.Size = new System.Drawing.Size(424, 60);
+            this.manageContractToolStripMenuItem.Text = "Manage Contract";
+            // 
+            // printToolStripMenuItem2
+            // 
+            this.printToolStripMenuItem2.Name = "printToolStripMenuItem2";
+            this.printToolStripMenuItem2.Size = new System.Drawing.Size(424, 60);
+            this.printToolStripMenuItem2.Text = "Print";
+            // 
+            // contractDetailToolStripMenuItem
+            // 
+            this.contractDetailToolStripMenuItem.Name = "contractDetailToolStripMenuItem";
+            this.contractDetailToolStripMenuItem.Size = new System.Drawing.Size(424, 60);
+            this.contractDetailToolStripMenuItem.Text = "Contract Detail";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(858, 566);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(880, 622);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Parking System";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem xeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem thợToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chuyênMônToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hợpĐồngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageVerhicleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem staToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vehicleListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageWorkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem workListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem manageAllWorkerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageContractToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem contractDetailToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addVehicleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteVehicleByIDToolStripMenuItem;
+    }
+}
+
---
+++ Parking-System/mainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parking_System
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void manageVerhicleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            manageVehiclesForm manageForm = new manageVehiclesForm();
+            manageForm.Show(this);
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            printVehiclesForm printForm = new printVehiclesForm();
+            printForm.Show(this);
+        }
+
+        private void addVehicleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            addVehicleForm addForm = new addVehicleForm();
+            addForm.Show(this);
+        }
+
+        private void deleteVehicleByIDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            deleteVehicleByID deleteForm = new deleteVehicleByID();
+            deleteForm.Show(this);
+        }
+
+        private void staToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            staticsForm stf = new staticsForm();
+            stf.ShowDialog();
+        }
+
+        private void vehicleListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            vehiclesListForm list = new vehiclesListForm();
+            list.ShowDialog();
+        }
+
+        private void editVehicleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            editVehiclesForm editForm = new editVehiclesForm();
+            editForm.Show(this);
+        }
+    }
+}
