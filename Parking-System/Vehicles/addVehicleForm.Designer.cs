af7a236fb971d7b0d2a23975829600507e86b23b
Lưu Văn Cụi <luuvancui10a1516@gmail.com>
2020-04-23 10:59:45 +0700

    add, edit, delete

---
+++ Parking-System/Vehicles/addVehicleForm.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace QuanLyNhaXe01
+{
+    partial class addVehicleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addVehicleForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonLoadLicensePlate = new System.Windows.Forms.Button();
+            this.radioButtonMoto = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelInTime = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.radioButtonBike = new System.Windows.Forms.RadioButton();
+            this.radioButtonCar = new System.Windows.Forms.RadioButton();
+            this.textBoxCardID = new System.Windows.Forms.TextBox();
+            this.buttonLoadModel = new System.Windows.Forms.Button();
+            this.buttonLoadUser = new System.Windows.Forms.Button();
+            this.buttonLoadVehiclePictrue = new System.Windows.Forms.Button();
+            this.dateTimePickerInTime = new System.Windows.Forms.DateTimePicker();
+            this.buttonAddVehicle = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.pictureBoxUser = new System.Windows.Forms.PictureBox();
+            this.pictureBoxVehiclePicture = new System.Windows.Forms.PictureBox();
+            this.pictureBoxModel = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLicensePlate = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVehiclePicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxModel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLicensePlate)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Name = "label1";
+            // 
+            // buttonLoadLicensePlate
+            // 
+            this.buttonLoadLicensePlate.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.buttonLoadLicensePlate, "buttonLoadLicensePlate");
+            this.buttonLoadLicensePlate.Name = "buttonLoadLicensePlate";
+            this.buttonLoadLicensePlate.UseVisualStyleBackColor = false;
+            this.buttonLoadLicensePlate.Click += new System.EventHandler(this.buttonLoadLicensePlate_Click);
+            // 
+            // radioButtonMoto
+            // 
+            resources.ApplyResources(this.radioButtonMoto, "radioButtonMoto");
+            this.radioButtonMoto.BackColor = System.Drawing.Color.Transparent;
+            this.radioButtonMoto.ForeColor = System.Drawing.Color.Lime;
+            this.radioButtonMoto.Name = "radioButtonMoto";
+            this.radioButtonMoto.TabStop = true;
+            this.radioButtonMoto.UseVisualStyleBackColor = false;
+            this.radioButtonMoto.CheckedChanged += new System.EventHandler(this.radioButtonMoto_CheckedChanged);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Name = "label5";
+            // 
+            // labelInTime
+            // 
+            resources.ApplyResources(this.labelInTime, "labelInTime");
+            this.labelInTime.BackColor = System.Drawing.Color.Transparent;
+            this.labelInTime.ForeColor = System.Drawing.Color.Lime;
+            this.labelInTime.Name = "labelInTime";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.ForeColor = System.Drawing.Color.Lime;
+            this.label8.Name = "label8";
+            // 
+            // radioButtonBike
+            // 
+            resources.ApplyResources(this.radioButtonBike, "radioButtonBike");
+            this.radioButtonBike.BackColor = System.Drawing.Color.Transparent;
+            this.radioButtonBike.ForeColor = System.Drawing.Color.Lime;
+            this.radioButtonBike.Name = "radioButtonBike";
+            this.radioButtonBike.TabStop = true;
+            this.radioButtonBike.UseVisualStyleBackColor = false;
+            this.radioButtonBike.CheckedChanged += new System.EventHandler(this.radioButtonBike_CheckedChanged);
+            // 
+            // radioButtonCar
+            // 
+            resources.ApplyResources(this.radioButtonCar, "radioButtonCar");
+            this.radioButtonCar.BackColor = System.Drawing.Color.Transparent;
+            this.radioButtonCar.ForeColor = System.Drawing.Color.Lime;
+            this.radioButtonCar.Name = "radioButtonCar";
+            this.radioButtonCar.TabStop = true;
+            this.radioButtonCar.UseVisualStyleBackColor = false;
+            this.radioButtonCar.CheckedChanged += new System.EventHandler(this.radioButtonCar_CheckedChanged_1);
+            // 
+            // textBoxCardID
+            // 
+            resources.ApplyResources(this.textBoxCardID, "textBoxCardID");
+            this.textBoxCardID.Name = "textBoxCardID";
+            // 
+            // buttonLoadModel
+            // 
+            this.buttonLoadModel.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.buttonLoadModel, "buttonLoadModel");
+            this.buttonLoadModel.Name = "buttonLoadModel";
+            this.buttonLoadModel.UseVisualStyleBackColor = false;
+            this.buttonLoadModel.Click += new System.EventHandler(this.buttonLoadModel_Click);
+            // 
+            // buttonLoadUser
+            // 
+            this.buttonLoadUser.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.buttonLoadUser, "buttonLoadUser");
+            this.buttonLoadUser.Name = "buttonLoadUser";
+            this.buttonLoadUser.UseVisualStyleBackColor = false;
+            this.buttonLoadUser.Click += new System.EventHandler(this.buttonLoadUser_Click);
+            // 
+            // buttonLoadVehiclePictrue
+            // 
+            this.buttonLoadVehiclePictrue.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.buttonLoadVehiclePictrue, "buttonLoadVehiclePictrue");
+            this.buttonLoadVehiclePictrue.Name = "buttonLoadVehiclePictrue";
+            this.buttonLoadVehiclePictrue.UseVisualStyleBackColor = false;
+            this.buttonLoadVehiclePictrue.Click += new System.EventHandler(this.buttonLoadVehiclePictrue_Click);
+            // 
+            // dateTimePickerInTime
+            // 
+            resources.ApplyResources(this.dateTimePickerInTime, "dateTimePickerInTime");
+            this.dateTimePickerInTime.Name = "dateTimePickerInTime";
+            // 
+            // buttonAddVehicle
+            // 
+            this.buttonAddVehicle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.buttonAddVehicle, "buttonAddVehicle");
+            this.buttonAddVehicle.Name = "buttonAddVehicle";
+            this.buttonAddVehicle.UseVisualStyleBackColor = false;
+            this.buttonAddVehicle.Click += new System.EventHandler(this.buttonAddVehicle_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.BackColor = System.Drawing.Color.Gray;
+            resources.ApplyResources(this.buttonCancel, "buttonCancel");
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // pictureBoxUser
+            // 
+            this.pictureBoxUser.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.pictureBoxUser, "pictureBoxUser");
+            this.pictureBoxUser.Name = "pictureBoxUser";
+            this.pictureBoxUser.TabStop = false;
+            // 
+            // pictureBoxVehiclePicture
+            // 
+            this.pictureBoxVehiclePicture.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.pictureBoxVehiclePicture, "pictureBoxVehiclePicture");
+            this.pictureBoxVehiclePicture.Name = "pictureBoxVehiclePicture";
+            this.pictureBoxVehiclePicture.TabStop = false;
+            // 
+            // pictureBoxModel
+            // 
+            this.pictureBoxModel.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.pictureBoxModel, "pictureBoxModel");
+            this.pictureBoxModel.Name = "pictureBoxModel";
+            this.pictureBoxModel.TabStop = false;
+            // 
+            // pictureBoxLicensePlate
+            // 
+            this.pictureBoxLicensePlate.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.pictureBoxLicensePlate, "pictureBoxLicensePlate");
+            this.pictureBoxLicensePlate.Name = "pictureBoxLicensePlate";
+            this.pictureBoxLicensePlate.TabStop = false;
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Name = "label6";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonBike);
+            this.groupBox1.Controls.Add(this.radioButtonCar);
+            this.groupBox1.Controls.Add(this.radioButtonMoto);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // addVehicleForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonAddVehicle);
+            this.Controls.Add(this.dateTimePickerInTime);
+            this.Controls.Add(this.buttonLoadVehiclePictrue);
+            this.Controls.Add(this.buttonLoadUser);
+            this.Controls.Add(this.pictureBoxUser);
+            this.Controls.Add(this.buttonLoadModel);
+            this.Controls.Add(this.pictureBoxVehiclePicture);
+            this.Controls.Add(this.pictureBoxModel);
+            this.Controls.Add(this.pictureBoxLicensePlate);
+            this.Controls.Add(this.textBoxCardID);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.labelInTime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonLoadLicensePlate);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "addVehicleForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVehiclePicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxModel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLicensePlate)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonLoadLicensePlate;
+        private System.Windows.Forms.RadioButton radioButtonMoto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelInTime;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RadioButton radioButtonBike;
+        private System.Windows.Forms.RadioButton radioButtonCar;
+        private System.Windows.Forms.TextBox textBoxCardID;
+        private System.Windows.Forms.PictureBox pictureBoxLicensePlate;
+        private System.Windows.Forms.PictureBox pictureBoxModel;
+        private System.Windows.Forms.PictureBox pictureBoxVehiclePicture;
+        private System.Windows.Forms.Button buttonLoadModel;
+        private System.Windows.Forms.PictureBox pictureBoxUser;
+        private System.Windows.Forms.Button buttonLoadUser;
+        private System.Windows.Forms.Button buttonLoadVehiclePictrue;
+        private System.Windows.Forms.DateTimePicker dateTimePickerInTime;
+        private System.Windows.Forms.Button buttonAddVehicle;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
